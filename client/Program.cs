f2d590e4bc4db4bd994d51bf7bdc8b751f806744
Farooq Mahmud <farooq.mahmud@marel.com>
2021-06-24 11:46:28 -0700

    wip

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace cert_client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("USAGE: cert-client [cert path] [api base address]");
+                return;
+            }
+
+            var certPath = args[0];
+            var apiBaseUri = args[1];
+
+            var cert = new X509Certificate2(certPath, "1234");
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(cert);
+            var client = new HttpClient(handler);
+
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"{apiBaseUri}/WeatherForecast"),
+                Method = HttpMethod.Get,
+            };
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var data = JsonDocument.Parse(responseContent);
+                Console.WriteLine(data);
+                return;
+            }
+
+            throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
         }
     }
 }
